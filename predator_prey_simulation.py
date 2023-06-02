7aa6ef04bc40d4f3ffd9334c2070da9151285775
Roger Salas <58913883+rogerjs93@users.noreply.github.com>
2023-06-02 14:47:53 +0300

    Update predator_prey_simulation.py

--- predator_prey_simulation.py
+++ predator_prey_simulation.py
@@ -1,39 +1,63 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-# Define the system parameters
-alpha = 1.0  # birth rate of prey
-beta = 0.1  # death rate of prey due to predation
-gamma = 1.5  # natural death rate of predators
-delta = 0.075  # rate at which predators increase due to predation
+class StochasticMultiSpeciesSimulation:
+    def __init__(self, species, relationships, steps=1000, noise_strength=0.1):
+        self.species = species
+        self.relationships = relationships
+        self.steps = steps
+        self.noise_strength = noise_strength
+        self.validate_parameters()
 
-# Initialize the populations
-prey_population = 40
-predator_population = 9
+    def validate_parameters(self):
+        if self.steps < 1:
+            raise ValueError("Number of steps must be greater than 0.")
+        if self.noise_strength < 0:
+            raise ValueError("Noise strength must be non-negative.")
+        for species, data in self.species.items():
+            if data['population'] < 0:
+                raise ValueError(f"Initial population of {species} must be non-negative.")
+            for other_species, relationship in self.relationships[species].items():
+                if relationship < 0 and other_species != species:
+                    raise ValueError(f"Relationship coefficient between {species} and {other_species} must be non-negative.")
 
-# Initialize lists to store population sizes at each time step
-prey_populations = [prey_population]
-predator_populations = [predator_population]
+    def run(self):
+        for _ in range(self.steps):
+            for species, data in self.species.items():
+                change = 0
+                for other_species, relationship in self.relationships[species].items():
+                    change += relationship * self.species[other_species]['population']
+                noise = np.random.normal(scale=self.noise_strength)
+                data['population'] += change + noise
+                data['history'].append(data['population'])
 
-# Define the number of time steps
-num_steps = 1000
+    def plot_results(self):
+        plt.figure(figsize=(12, 6))
+        for species, data in self.species.items():
+            plt.plot(data['history'], label=species)
+        plt.legend(loc='upper right')
+        plt.xlabel('Time')
+        plt.ylabel('Population')
+        plt.title('Stochastic Multi-Species Dynamics')
+        plt.show()
 
-# Run the simulation
-for _ in range(num_steps):
-    new_prey_population = prey_population + (alpha*prey_population - beta*prey_population*predator_population)
-    new_predator_population = predator_population + (delta*prey_population*predator_population - gamma*predator_population)
+    def save_results(self, filename):
+        df = pd.DataFrame(self.species)
+        df.to_csv(filename)
 
-    prey_populations.append(new_prey_population)
-    predator_populations.append(new_predator_population)
+if __name__ == "__main__":
+    species = {
+        'prey': {'population': 40, 'history': [40]},
+        'predator': {'population': 9, 'history': [9]}
+    }
 
-    prey_population, predator_population = new_prey_population, new_predator_population
+    relationships = {
+        'prey': {'prey': 1.0, 'predator': -0.1},
+        'predator': {'prey': 0.075, 'predator': -1.5}
+    }
 
-# Plot the results
-plt.figure(figsize=(12, 6))
-plt.plot(prey_populations, label='Prey')
-plt.plot(predator_populations, label='Predator')
-plt.legend(loc='upper right')
-plt.xlabel('Time')
-plt.ylabel('Population')
-plt.title('Predator-Prey Dynamics')
-plt.show()
+    sim = StochasticMultiSpeciesSimulation(species, relationships, noise_strength=0.2)
+    sim.run()
+    sim.plot_results()
+    sim.save_results('simulation_results.csv')
